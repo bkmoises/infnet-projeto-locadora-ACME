35a06bb702e94cf497b810496d23696aef8434bd
bkmoises <bk_moises@hotmail.com>
2024-09-11 18:09:25 -0300

    feature: inclusão de serviço para resiliência do projeto

--- catalogo/src/main/java/com/acme/catalogo/service/AvaliacaoService.java
+++ catalogo/src/main/java/com/acme/catalogo/service/AvaliacaoService.java
@@ -2,17 +2,31 @@ package com.acme.catalogo.service;
 
 import com.acme.catalogo.client.AvaliacaoClient;
 import com.acme.catalogo.model.Avaliacao;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 @Service
 @RequiredArgsConstructor
 public class AvaliacaoService {
     private final AvaliacaoClient avaliacaoClient;
+    private static Map<Long, List<Avaliacao>> CACHE = new ConcurrentHashMap<>();
 
+    @CircuitBreaker(name = "avaliacaoService", fallbackMethod = "findOnCache")
     public List<Avaliacao> getAllById(Long catalogoId) {
-        return avaliacaoClient.getById(catalogoId);
+        List<Avaliacao> avaliacoes = avaliacaoClient.getById(catalogoId);
+        CACHE.put(catalogoId, avaliacoes);
+        return avaliacoes;
     }
+
+    private List<Avaliacao> findOnCache(Long catalogoId, Throwable throwable) {
+        return CACHE.getOrDefault(catalogoId, new ArrayList<>());
+    }
+
+
 }
